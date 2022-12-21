458b3df5a39a8742285fadfefa90d741d201a11a
Oyal2 <oyalfokshner@gmail.com>
2022-12-21 02:15:31 -0500

    check fix

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/cypher" element={<HomePage />} />
       <Route path="/cypher/signin" element={<SignIn />} />
